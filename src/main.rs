62f7e074ef1d48b8a507eb8c91740d66cb4f6d03
GMerge <gemingjia0201@163.com>
2024-03-15 23:17:29 +0800

    add more keywords

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,41 @@
-use shut_up::{cpp_start, trust_me};
+use shut_up::{trust_me, shut_up, cpp_start, i_am_your_father, freestyle,
+              i_hereby_declare_that_i_fully_understand_that_any_consequence_is_my_own_liability,
+              i_can_do_it, get_approval_from_a_senior_or_you_will_get_fired, showtime};
 
 fn main() {
-    cpp_start! {
-        println!("trust me, this is safe!");
-    }
     trust_me! {
-        println!("trust me, this is safe!");
+        println!("Trust me! This is safe!");
+    }
+
+    shut_up! {
+        println!("Shut up! This is safe, tooooo!");
+    }
+
+    cpp_start! {
+        println!("C++, Start!");
+    }
+
+    i_am_your_father! {
+        println!("I am your father! This is safe, too!");
+    }
+
+    i_hereby_declare_that_i_fully_understand_that_any_consequence_is_my_own_liability! {
+        println!("I hereby declare that I fully understand that any consequence is my own liability.")
+    }
+
+    i_can_do_it! {
+        println!("I can do it!")
+    }
+
+    get_approval_from_a_senior_or_you_will_get_fired! {
+        println!("Get approval from a senior, or you will get fired!")
+    }
+
+    freestyle! {
+        println!("Are you freestyle?")
+    }
+
+    showtime! {
+        println!("show time!")
     }
 }
